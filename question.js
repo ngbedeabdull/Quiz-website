8cc717ae8b9ab43d85b8469f7bf49c3b90f5e6d6
ngbedeabdull <ngbedeabdull@gmail.com>
2026-01-08 03:21:58 -0800

    Add files via upload

---
+++ question.js
@@ -0,0 +1,61 @@
+let questions = [
+    {
+        numb: 1,
+        question: "what does HTML stand for?",
+        answer: "C. hyper text markup language",
+        options: [
+            "A. hyper type multi language",
+            "B. hyper text multiple language",
+            "C. hyper text markup language",
+            "D. home text multi language",
+        ]
+    },
+
+    {
+        numb: 2,
+        question: "what does CSS stand for?",
+        answer: "A. cascading style sheet",
+        options: [
+            "A. cascading style sheet",
+            "B. cute style sheet",
+            "C. computer style sheet",
+            "D. cohedal style sheet",
+        ]
+    },
+
+    {
+        numb: 3,
+        question: "what does PHP stand for?",
+        answer: "A. hypertext preprocessor",
+        options: [
+            "A. hypertext preprocessor",
+            "B. hometext progrming",
+            "C. hypertext preprograming",
+            "D. programing hypertext preprocessor"
+        ]
+    },
+
+    {
+        numb: 4,
+        question: "what does SQL stand for?",
+        answer: "D. structured query language",
+        options: [
+            "A. strength query language",
+            "B. stylesheet query language",
+            "C. science question language",
+            "D. structured query language",
+        ]
+    },
+
+    {
+        numb: 5,
+        question: "what does XML stand for?",
+        answer: "D. extensible markup language",
+        options: [
+            "A. excellent multiple language",
+            "B. explore multiple language",
+            "C. extra markup language",
+            "D. extensible markup language",
+        ]
+    },
+];
